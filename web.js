b4a6a74e6ecc6e067dd7628162b9efa3191df9ed
Minh Pham <blah@blah.com>
2013-07-10 20:30:55 +1000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var index = 'index.html';
+var content = fs.readFileSync(index);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content.toString);
 });
 
 var port = process.env.PORT || 5000;
